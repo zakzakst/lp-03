54bdd81b1346b9fcfc2cf532a4a52a023e3f1e14
satoshi ishizaki <e1433104@yahoo.co.jp>
2020-08-05 13:47:17 +0900

    ギャラリーセクションの作成

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,5 @@
-// import $ from 'jquery';
-// import { voicesFunc } from './common/voices';
-// import { pageLoaderFunc } from './common/page-loader';
-// import { goTopFunc } from './common/go-top';
-// import { galleryFunc } from './common/gallery';
-// import { cycleImageFunc } from './common/cycle-image';
-// import { countUpFunc } from './common/count-up';
-// import { headerFunc } from './common/header';
+import { galleryAnim } from './module/gallery';
 
-// $(function() {
-//   voicesFunc();
-//   pageLoaderFunc();
-//   goTopFunc();
-//   galleryFunc();
-//   cycleImageFunc();
-//   countUpFunc();
-//   headerFunc();
-//   new WOW().init();
-// });
+(function() {
+  galleryAnim();
+})();
